d700e18570d001bba9e41005754ff1af5ae55687
Padmashree Jha <codewithpom@gmail.com>
2021-11-01 11:39:10 +0530

    Cleaned everything

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 app.use('/', require('./routes/upload')); // add the upload route
-app.use('/', require('./routes/download'));
 
 // start the server
 app.listen(3000, () => {
