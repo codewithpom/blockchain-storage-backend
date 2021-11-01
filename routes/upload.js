c2542c9014ed697dc9f1427c17f967c0db0f4056
Padmashree Jha <codewithpom@gmail.com>
2021-11-01 07:41:11 +0000

    Added cors

--- routes/upload.js
+++ routes/upload.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const cors = require('cors');
 const bodyParser = require('body-parser')
 const multer = require('multer');
 const upload = multer();
 const { saveToIpfsWithFilename } = require('../functions/upload_file');
 
 
-// add cors support
-router.use(cors());
 router.use(bodyParser.json()) // for parsing application/json
 router.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
@@ -17,6 +14,8 @@ router.use(bodyParser.urlencoded({ extended: true })) // for parsing application
 router.post('/upload', upload.array('file'), async (req, res) => {
     const file_buffer = req.files[0].buffer;
     const file_name = req.files[0].originalname;
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type, Accept");
     saveToIpfsWithFilename(file_buffer, file_name, res);
 
 
