d7cfb39fd15b1892494ef72d59217fb87b1d4174
Padmashree Jha <codewithpom@gmail.com>
2021-11-01 11:49:52 +0530

    Deployment to vercel

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 app.use('/', require('./routes/upload')); // add the upload route
+const PORT = process.env.PORT || 8080;
 
 // start the server
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
 
 
